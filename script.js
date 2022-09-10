62ad1fb2d92aacc70f2018ec4fe2664ebd397f20
jessymatta <jessy.w.matta@gmail.com>
2022-09-10 17:12:54 +0300

    back-end: fetched Age and Gender predictions and displayed them on the page

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const dog_img=document.getElementById("background-image");
+const dog_img = document.getElementById("background-image");
+
 
 
 // A function that retrieves dog pics from an api and changes the background image of the page
@@ -6,10 +7,49 @@ function fetchDogPics() {
     fetch('https://dog.ceo/api/breeds/image/random').then((data) => {
         return data.json();
     }).then((completedata) => {
-        dog_img.style.background="linear-gradient(63.71deg, rgba(0, 0, 0, 0.9) 3.36%, rgba(0, 0, 0, 0) 86.41%), linear-gradient(243.59deg, #1A1F91 10.24%, rgba(36, 42, 192, 0) 100%), url("+completedata.message+")";
-        dog_img.style.backgroundRepeat="no-repeat";
-        dog_img.style.backgroundSize="cover";
+        dog_img.style.background = "linear-gradient(63.71deg, rgba(0, 0, 0, 0.9) 3.36%, rgba(0, 0, 0, 0) 86.41%), linear-gradient(243.59deg, #1A1F91 10.24%, rgba(36, 42, 192, 0) 100%), url(" + completedata.message + ")";
+        dog_img.style.backgroundRepeat = "no-repeat";
+        dog_img.style.backgroundSize = "cover";
     });
+}
+
+//----------------------------------------------------------------------------------------------------
+// Function that gets gender prediction according to a certain name and displays it on the webpage
+function fetchGenderPredic(name_input) {
+    fetch(`https://api.genderize.io?name=${name_input}`).then((data) => {
+        return data.json();
+    }).then((completedata) => {
+        console.log(completedata.gender);
+        document.getElementById("gender-pred").innerHTML = completedata.gender;
+    });
+}
+
+
+//Function that gets age prediction according to a certain name and displays it on the webpage
+function fetchAgePredic(name_input) {
+    fetch(`https://api.agify.io/?name=${name_input}`).then((data) => {
+        return data.json();
+    }).then((completedata) => {
+        document.getElementById("age-pred").innerHTML = completedata.age;
+    });
+}
+//----------------------------------------------------------------------------------------------------
+
+// A function that returns the inputed name
+// let name_to_fetch;
+function getName() {
+
+    document.getElementById("submit-btn").onclick = function () {
+       var  name_to_fetch = document.getElementById("user_name").value; //to ask about this one
+        console.log("NAMEEEEEE " + name_to_fetch);
+        fetchGenderPredic(name_to_fetch);
+        fetchAgePredic(name_to_fetch);
+
+
+
+
+    }
+
 }
 
 
@@ -17,3 +57,4 @@ function fetchDogPics() {
 
 // ---------------------  Functions call --------------------------------------
 fetchDogPics();
+getName();
