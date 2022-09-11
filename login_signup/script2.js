9226424ccd25c8a38987839fd2b902de91c9636d
jessymatta <jessy.w.matta@gmail.com>
2022-09-11 22:44:02 +0300

    back-end: made the sign in and sign up pages functional

---
+++ login_signup/script2.js
@@ -0,0 +1,40 @@
+const signUp = e => {
+    let fname = document.getElementById('fname').value,
+        lname = document.getElementById('lname').value,
+        email = document.getElementById('email').value,
+        pwd = document.getElementById('pwd').value;
+
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+
+    let exist = formData.length && 
+        JSON.parse(localStorage.getItem('formData')).some(data => 
+            data.fname.toLowerCase() == fname.toLowerCase() && 
+            data.lname.toLowerCase() == lname.toLowerCase()
+        );
+
+    if(!exist){
+        formData.push({ fname, lname, email, pwd });
+        localStorage.setItem('formData', JSON.stringify(formData));
+        document.querySelector('form').reset();
+        document.getElementById('fname').focus();
+        document.getElementById('acc-created').innerHTML ="Account Created.\n\n Sign In using the link above."
+    }
+    else{
+        document.getElementById('acc-created').innerHTML ="You already signed up";
+    }
+    e.preventDefault();
+}
+
+function signIn(e) {
+    let email = document.getElementById('email').value, pwd = document.getElementById('pwd').value;
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+    let exist = formData.length && 
+    JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd);
+    if(!exist){
+        document.getElementById('acc-log').innerHTML ="You already signed up";
+    }
+    else{
+        location.href = "/";
+    }
+    e.preventDefault();
+}
